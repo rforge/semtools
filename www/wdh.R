b23850980f7bd28e6d1f1a47e5f6f540b6354ee5
ecmerkle <ecmerkle@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-11 01:00:32 +0000

    Addressed more reviewer comments, including:   - Added commentary on the n=50 simulation condition   - Addressed the issue of identification conditions impacting     the test results
    Also added a draft set of responses to reviewers in the rev1 directory.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/semtools@74 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/wdh.R
+++ www/wdh.R
@@ -37,19 +37,45 @@ wdhfit <- function(data, silent = TRUE, suppressWarnings = TRUE, ...)
   return(rval)
 }
 
-#coef.wdhfit <- function(object, ...) {
-#  switch(object$engine,  
-#  "OpenMx" = {
-#    n <- object$names
-#    cf <- object$model@output$estimate
-#    cf <- cf[n[,1]]
-#    names(cf) <- n[,2]
-#    cf
-#  },
-#  "lavaan" = {
-#    unclass(getMethod("coef", "lavaan")(object$model, ...))
-#  })
-#}
+wdhfit2 <- function(data, silent = TRUE, suppressWarnings = TRUE, ...)
+{
+  ## Fit the same model but with different identification constraints.
+  ## data checks
+  data <- as.data.frame(data)
+  stopifnot(c("VERBREA","ABSREA","NUMER","gpa","grouping") %in% names(data))
+  data <- data[, c("VERBREA","ABSREA","NUMER","gpa","grouping")]
+
+  ## require package
+  stopifnot(require("lavaan"))
+  
+  ## model from Step 5b of Wicherts et al., 2005:
+  mod5b <- 'f1 =~ label(rep("l3",4))*VERBREA + 1*ABSREA + label(c("l1","l1","l1","l1expmin"))*NUMER 
+                  f1 ~ label(c("a","faccontmin","facexpmaj","facexpmin"))*1 + c(0,NA,NA,NA)*1
+                  f1 ~~ label(c("vmaj","vmin","vmaj","vmin"))*f1
+                  NUMER ~~ label(c("e1","e1","e1","e1expmin"))*NUMER
+                  ABSREA ~~ label(rep("e2",4))*ABSREA
+                  VERBREA ~~ label(rep("e3",4))*VERBREA
+
+                  NUMER ~ label(c("meanNA","meanNA","meanNA","meanNAexpmin"))*1
+                  ABSREA ~ label(c("meanARcontmaj","meanARcontmin","meanAR","meanAR"))*1
+                  VERBREA ~ label(rep("meanVR",4))*1'
+
+  ## fit model
+  rval <- lavaan(mod5b, data=data, meanstructure=TRUE, group="grouping")
+
+  ## store (1) data, (2) fitted model object, (3) grouping variable,
+  ## (4) focal group nubmers (group ordering is defined by lavaan and
+  ##                          can be found in rval@Sample@group.label)
+  rval <- list(
+    data = data[,1:3],
+    model = rval,
+    groupvar = data$grouping,
+    groupfoc = 4
+  )
+  class(rval) <- "wdhfit"
+
+  return(rval)
+}
 
 logLik.wdhfit <- function(object, ...) {
   rval <- as.vector(fitMeasures(object$model, "logl"))
