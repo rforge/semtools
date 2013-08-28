2daa9eb73436739a7f2a9f6f5cad9be4da44b9b9
tingwang <tingwang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-28 20:40:11 +0000

    update simall-ordinal. R file. The code is ready to run full simulation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/semtools@246 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- frontiers/simall-ordinal.R
+++ frontiers/simall-ordinal.R
@@ -1,7 +1,7 @@
 
 ## Data-generating process
 set.seed=1090
-dgp <- function(nobs = 240, diff = 1.5, nlevels = 3, gradual=FALSE, anomaly=FALSE, parms="loadings")
+dgp1 <- function(nobs = 240, diff = 1.5, nlevels = 3, gradual=FALSE, anomaly=FALSE, parms="loadings")
 {
   ## Generates data from a factor analysis model that violates
   ## measurement invariance.  Also generates an ordinal auxiliary
@@ -116,16 +116,133 @@ dgp <- function(nobs = 240, diff = 1.5, nlevels = 3, gradual=FALSE, anomaly=FALS
   cbind(as.data.frame(datmat), age)
 }
 
+dgp2 <-function(nobs = 240, diff = 1.5, nlevels = 3, gradual=FALSE, anomaly=FALSE,
+                parms="loadings")
+{
+  ## Generates data from a factor analysis model that violates
+  ## measurement invariance.  Also generates an ordinal auxiliary
+  ## variable related to the invariance (loosely called "age").
+
+  ## If gradual==TRUE, mild instability starts halfway up age
+  ## and gets larger.  If gradual==FALSE, the instability starts
+  ## halfway up age and stays constant.
+  ## If anomaly==TRUE, instability only occurs at level (1+nlevels/2).
+  ## Otherwise, instability continues up the ordinal variable.
+  stopifnot(require("mvtnorm"))
+  
+  # ses is SE increase in parameters at last ordinal category.
+  # (This is constant if gradual==FALSE but not if gradual==TRUE.)
+  ses <- diff
+  sampsize <- nobs
+  sampsize.per.nobs <- nobs %/% nlevels
+
+  ## Deal with cases where nobs/nlevels is not an integer
+  age <- rep(NA, sampsize)
+  age[1:(nlevels*sampsize.per.nobs)] <- rep(1:nlevels, each=sampsize.per.nobs)
+  if (any(is.na(age))) age[is.na(age)] <- sample((1:nlevels),sum(is.na(age)))
+  half.level <- ifelse(nlevels %% 2 == 1, (nlevels %/% 2)+1, nlevels/2)
+  
+  # Define parameter vectors/matrices:
+  theta<-c(4.92,2.96,5.96,1,3.24,4.32,7.21,26.77,13.01,30.93,3.17,8.82,22.5,-0.48,
+           29.32,24.70,14.84,10.59,19.30,18.01)
+  asym<-c(10.037376,4.978146,8.868835,9.009867,3.095191,4.501605,
+           7.371680,60.969079,24.444897,78.555777,8.298542,17.701359,
+          47.210688,1.127309,6.874096,4.666005,8.151785,3.696972,
+           5.242366,8.630417)
+  mu<-theta[15:20]
+  # Loadings:
+  lambda <- matrix(0,6,2)
+  lambda[,1] <- c(theta[1:3],0,0,0)
+  lambda[,2] <- c(theta[4],0,0,theta[5:7])
+
+  phi <- matrix(theta[14],2,2)
+  diag(phi) <- 1
+
+  ## Base variances
+  psi <- matrix(0,6,6)
+  diag(psi) <- theta[8:13]
+
+  ## Initialize data matrix:
+  datmat <- matrix(NA,sampsize,6)
+  colnames(datmat) <- c("x1", "x2", "x3", "y1", "y2", "y3")
+
+  ## Generate z and u vectors for invariant individuals:
+  n.invariant <- sum(age <= half.level)
+  z <- t(rmvnorm(n.invariant,rep(0,2),phi))
+  u <- t(rmvnorm(n.invariant,rep(0,6),psi))
+
+  ## Based on z and u, generate data:
+  tmp.ind <- which(age <= half.level)
+  for (i in 1:n.invariant){
+    datmat[tmp.ind[i],] <- mu + lambda%*%z[,i] + u[,i]
+  }
+
+  ## Now do the same for invariant-violating levels
+  n.vary <- nlevels - half.level
+  each.vary <- ses/n.vary
+  for (i in (half.level+1):nlevels){
+    tmp.n <- sum(age==i)
+    tmp.ind <- which(age==i)
+
+  if(parms=="extra"){parmsvector=c(rep(0,3),1,rep(0,3),rep(0,6),0,rep(0,6))}
+  if(parms=="extra+loading"){parmsvector=c(1,rep(0,2),0,rep(0,3),rep(0,6),0,rep(0,6))}
+  if(parms=="extra+var"){parmsvector=c(rep(0,3),0,rep(0,3),rep(0,6),1,rep(0,6))}
+  if(parms=="extra+error"){parmsvector=c(rep(0,3),0,rep(0,3),1,rep(0,5),0,rep(0,6))}
+   
+    mult <- ses
+    if (anomaly) mult <- ifelse(i==(half.level+1), ses, 0)
+
+    ## SES found by fitting model to population S, multiplying
+    ## observed SES by sqrt(n)
+    if (gradual){
+      if (anomaly) stop("gradual=TRUE and anomaly=TRUE do not work together.")
+    theta.tmp <- theta + (each.vary*asym*parmsvector/sqrt(tmp.n))
+    # Loadings:
+    tmp.lambda <- matrix(0,6,2)
+    tmp.lambda[,1] <- c(theta.tmp[1:3],0,0,0)
+    tmp.lambda[,2] <- c(theta.tmp[4],0,0,theta.tmp[5:7])
+
+    tmp.phi <- matrix(theta.tmp[14],2,2)
+    diag(tmp.phi) <- 1
+
+    # variances
+    tmp.psi <- matrix(0,6,6)
+    diag(tmp.psi) <- theta.tmp[8:13]
+    } else {
+    theta.tmp <- theta + (mult*asym*parmsvector/sqrt(tmp.n))
+    # Loadings:
+    tmp.lambda <- matrix(0,6,2)
+    tmp.lambda[,1] <- c(theta.tmp[1:3],0,0,0)
+    tmp.lambda[,2] <- c(theta.tmp[4],0,0,theta.tmp[5:7])
+
+    tmp.phi <- matrix(theta.tmp[14],2,2)
+    diag(tmp.phi) <- 1
+
+    # variances
+    tmp.psi <- matrix(0,6,6)
+    diag(tmp.psi) <- theta.tmp[8:13]  
+    }
+    
+    u.tmp <- t(rmvnorm(tmp.n,rep(0,6),tmp.psi))
+    z.tmp <- t(rmvnorm(tmp.n,rep(0,2),tmp.phi))
+    for (j in 1:tmp.n){
+      datmat[tmp.ind[j],] <- mu + tmp.lambda%*%z.tmp[,j] + u.tmp[,j]
+    }
+  }
+  
+  cbind(as.data.frame(datmat), age)
+}
+
 ## Evaluate power simulation on a single dgp() scenario
 testpower <- function(nrep = 5000, size = 0.05, ordfun = NULL, parnum = parnum, test = test,
-                      verbose = TRUE, ...)
+                      sim="sim1",verbose = TRUE, ...)
 {
   pval <- matrix(rep(NA, length(test) * nrep), ncol = length(test))
   colnames(pval) <- test
+  dfun <- paste("dgp", substr(sim,4,4), sep="")
   
   for(i in 1:nrep) {
-    d <- dgp(...)
-
+    d <- do.call(dfun, list(...))
     mz <- ordfit(d)
 
     ## Requires estfun.lavaan
@@ -161,7 +278,7 @@ simulation <- function(diff = seq(0, 4, by = 0.25),parms=c("error","loading","va
   prs <- expand.grid(diff = diff, nlevels = nlevels, nobs = nobs, parms = parms)
   nprs <- nrow(prs)
 
-  parnum <- c(1,13,7)
+  parnum <- c(1,13,7,"1:6","7:12")
 
   tname <- c("ordmax","ordwmax","catdiff") # had suplm here
   
@@ -245,31 +362,24 @@ source("../www/mz-ordinal.R")
 source("../www/estfun-lavaan.R")
 source("../www/efpFunctional-cat.R")
 source("simall-ordinal.R")
-simtry <- simulation(nobs=c(480),nrep=300, diff=seq(0, 1.5, by = 0.25),
+
+## seed for replication
+  RNGkind(kind = "default", normal.kind = "default")
+  set.seed(1090)
+  
+
+  
+## To get a shorter version (say, 20 minutes)to see what's going on, you could do:
+simtry <- simulation(sim=c("sim1"),nobs=c(480),nrep=300, diff=seq(0, 4, by = 1),
                      parms=c("loading","error","var"))
-sim1 <-simulation()
+simtry <- simulation(sim=c("sim2"), nobs=c(480),nrep=300, diff=seq(0, 4, by = 1),
+                     parms=c("extra","extra+loading","extra+var","extra+error"))
 save(simtry,file ="simtry.rda")
 
-
-
-#load data file and give names for different levels in each variable.
-if(file.exists("simtry.rda")) {
-  load("simtry.rda")
-} else {
-  simtry <- simulation()
-  simtry$nlevels <- factor(simtry$nlevels)
-  levels(simtry$nlevels) <- paste("m=",levels(simtry$nlevels),sep="")
-  simtry$nobs <- factor(simtry$nobs)
-  levels(simtry$nobs) <- paste("n=",levels(simtry$nobs),sep="")
-  levels(simtry$test) <- c("ordmax","ordwmax","catdiff")
-  save(simtry, file="simtry.rda")
-}
-simtry$test <- factor(as.character(simtry$test),
-  levels = c("ordmax","ordwmax","catdiff"),
-  labels = c("ordmax","ordwmax","catdiff"))
-   simtry$nlevels <- factor(simtry$nlevels)
- levels(simtry$nlevels) <- paste("m=",levels(simtry$nlevels),sep="")
-
+## run full simulation
+sim1 <- simulation(sim=c("sim1"), nobs=c(480),parms=c("loading","error","var"))
+sim2 <- simulation(sim=c("sim2"), nobs=c(480),
+                   parms=c("extra","extra+loading","extra+var","extra+error"))
 
 library(lattice)
 #parm=loading
@@ -299,6 +409,73 @@ dev.off()
 postscript("error.pdf")
 simtry.tmp <- simtry[simtry$test %in% c("ordmax","ordwmax","catdiff")& simtry$nobs %in%
                      c(480)&simtry$parms %in% c("error"),]
+simtry.tmp$test <- factor(simtry.tmp$test)
+trellis.par.set(theme = canonical.theme(color = FALSE))
+mykey <- simpleKey(levels(simtry.tmp$test), points = TRUE, lines = TRUE)
+xyplot(power ~ diff | nlevels + pars + parms, group = ~ test, data = simtry.tmp, 
+             type = "b", xlab="Violation Magnitude", ylab="Power", key=mykey)
+dev.off()
+
+
+
+#load data file and give names for different levels in each variable.
+if(file.exists("simtry.rda")) {
+  load("simtry.rda")
+} else {
+  simtry <- simulation()
+  simtry$nlevels <- factor(simtry$nlevels)
+  levels(simtry$nlevels) <- paste("m=",levels(simtry$nlevels),sep="")
+  simtry$nobs <- factor(simtry$nobs)
+  levels(simtry$nobs) <- paste("n=",levels(simtry$nobs),sep="")
+  levels(simtry$test) <- c("ordmax","ordwmax","catdiff")
+  save(simtry, file="simtry.rda")
+}
+simtry$test <- factor(as.character(simtry$test),
+  levels = c("ordmax","ordwmax","catdiff"),
+  labels = c("ordmax","ordwmax","catdiff"))
+   simtry$nlevels <- factor(simtry$nlevels)
+ levels(simtry$nlevels) <- paste("m=",levels(simtry$nlevels),sep="")
+
+
+library(lattice)
+#parm=loading
+postscript("extra.pdf")
+simtry.tmp <- simtry[simtry$test %in% c("ordmax","ordwmax","catdiff")& simtry$nobs %in%
+                     c(480)&simtry$parms %in% c("extra"),]
+simtry.tmp$test <- factor(simtry.tmp$test)
+trellis.par.set(theme = canonical.theme(color = FALSE))
+mykey <- simpleKey(levels(simtry.tmp$test), points = TRUE, lines = TRUE)
+xyplot(power ~ diff | nlevels + pars + parms, group = ~ test, data = simtry.tmp, 
+             type = "b", xlab="Violation Magnitude", ylab="Power", key=mykey)
+dev.off()
+
+
+#parm=var
+postscript("extra+loading.pdf")
+simtry.tmp <- simtry[simtry$test %in% c("ordmax","ordwmax","catdiff")& simtry$nobs %in%
+                     c(480)&simtry$parms %in% c("extra+loading"),]
+simtry.tmp$test <- factor(simtry.tmp$test)
+trellis.par.set(theme = canonical.theme(color = FALSE))
+mykey <- simpleKey(levels(simtry.tmp$test), points = TRUE, lines = TRUE)
+xyplot(power ~ diff | nlevels + pars + parms, group = ~ test, data = simtry.tmp, 
+             type = "b", xlab="Violation Magnitude", ylab="Power", key=mykey)
+dev.off()
+
+#parm=error
+postscript("extra+var.pdf")
+simtry.tmp <- simtry[simtry$test %in% c("ordmax","ordwmax","catdiff")& simtry$nobs %in%
+                     c(480)&simtry$parms %in% c("extra+var"),]
+simtry.tmp$test <- factor(simtry.tmp$test)
+trellis.par.set(theme = canonical.theme(color = FALSE))
+mykey <- simpleKey(levels(simtry.tmp$test), points = TRUE, lines = TRUE)
+xyplot(power ~ diff | nlevels + pars + parms, group = ~ test, data = simtry.tmp, 
+             type = "b", xlab="Violation Magnitude", ylab="Power", key=mykey)
+dev.off()
+
+#parm=error
+postscript("extra+error.pdf")
+simtry.tmp <- simtry[simtry$test %in% c("ordmax","ordwmax","catdiff")& simtry$nobs %in%
+                     c(480)&simtry$parms %in% c("extra+error"),]
 simtry.tmp$test <- factor(simtry.tmp$test)
 trellis.par.set(theme = canonical.theme(color = FALSE))
 mykey <- simpleKey(levels(simtry.tmp$test), points = TRUE, lines = TRUE)
