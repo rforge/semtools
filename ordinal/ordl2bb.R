86b97d57055d48d346ebe229a77acad5a174460e
ecmerkle <ecmerkle@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-23 03:48:45 +0000

    ordl2bb sim, vector values of nproc are working
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/semtools@370 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- ordinal/ordl2bb.R
+++ ordinal/ordl2bb.R
@@ -2,29 +2,34 @@ simordl2bb <- function(freq, nproc, nrep, ...){
   ## assumes freq includes the final cumulative proportion of 1
   nbin <- length(freq) - 1L
   bp <- cumsum(freq)[1L:nbin]
+  maxproc <- max(nproc)
 
   ## find multivariate normal covariance matrix between bins
   cmat <- matrix(NA, nbin, nbin)
   for(i in 1L:nbin) cmat[i:nbin, i] <- cmat[i, i:nbin] <- sqrt(bp[i] * (1 - bp[i:nbin]))/sqrt((1 - bp[i]) * bp[i:nbin])
 
   ## create block-diagonal matrix so we can simulate all parameters at once
-  fullcmat <- matrix(0, nbin * nproc, nbin * nproc)
-  for(j in 1L:nproc) fullcmat[(nbin * (j - 1L) + 1L):(nbin * j), (nbin * (j - 1L) + 1L):(nbin * j)] <- cmat
+  fullcmat <- matrix(0, nbin * maxproc, nbin * maxproc)
+  for(j in 1L:maxproc) fullcmat[(nbin * (j - 1L) + 1L):(nbin * j), (nbin * (j - 1L) + 1L):(nbin * j)] <- cmat
 
   draws <- rmvnorm(nrep, sigma = fullcmat)
 
   ## now get L2 norm sequentially for each bin:
   ## columns 1,(nbin+1),(2nbin+1),...
   ## columns 2,(nbin+2),(2nbin+2),...
-  ## TODO if we also loops through values of colnums,
-  ##      (first=1, then=2, ..., nproc)
-  ##      we can simulate multiple values of nproc
-  colnums <- 1L:nproc
-  ssbin <- matrix(NA, nrep, nbin)
-  for(j in 1L:nbin) ssbin[, j] <- rowSums(as.matrix(draws[, (colnums - 1L) * nbin + j])^2)
+  colnums <- 1L:maxproc
+  ## One list entry per value of nproc
+  ssbins <- matrix(NA, nrep, length(nproc))
+  for(i in 1L:length(nproc)){
+    tmpbin <- matrix(NA, nrep, nbin)
+    for(j in 1L:nbin){
+      cols <- ((colnums - 1L) * nbin + j)[1:nproc[i]]
+      tmpbin[, j] <- rowSums(as.matrix(draws[, cols])^2)
+    }
+    ssbins[,i] <- apply(tmpbin, 1L, max)
+  }
 
-  ## now get max across the above matrix
-  apply(ssbin, 1L, max)
+  ssbins
 }
 
 if(FALSE){
@@ -36,7 +41,7 @@ if(FALSE){
   load("~/Projects/semtools/ordinal/critvals.rda")
   
   ## Define quantiles, compare to new simulation
-  qs <- (1:19)/20
+  qs <- c((1:19)/20, .975, .99)
   tmp <- simordl2bb(rep((1/8),8), 3, 50000)
   oldsim <- cval[[2]]$computeCritval(1-qs,3)
   newsim <- quantile(tmp, qs)
@@ -60,11 +65,25 @@ if(FALSE){
   cbind(oldsim, newsim)
 
   ## should match ordwmax when nproc==1
-  ## seems to be a slight difference, but closer
-  ## if there are equal bin proportions
   tmp <- simordl2bb(c(.2,.5,.15,.15), 1, 50000)
   tmp2 <- ordwmax(c(.2,.5,.15,.15))
   oldsim <- tmp2$computeCritval(.05,1)
   newsim <- quantile(tmp, .95)
   cbind(oldsim^2, newsim)
+
+  ## multiple values of nproc: 50000 takes awhile
+  ## for nproc=1:20, but could probably get away with a
+  ## smaller value of nrep.
+  tmp <- simordl2bb(rep((1/20),20), 1:20, 50000)
+  ## Comparison for each value of nproc
+  for(i in 1:20){
+    print(rbind(quantile(tmp[,i], qs[15:19]), cval[[4]]$computeCritval(1-qs[15:19], i)))
+  }
+
+  ## Try irregular values of nproc
+  npcs <- c(3,7,9)
+  tmp <- simordl2bb(rep((1/20),20), npcs, 50000)
+  for(i in 1:length(npcs)){
+    print(rbind(quantile(tmp[,i], qs[15:19]), cval[[4]]$computeCritval(1-qs[15:19], npcs[i])))
+  }
 }
